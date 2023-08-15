c143d0f6ad9a2d71621477c107cd2c67f383e6d7
Novak <novaldix@gmail.com>
2023-08-15 22:23:54 +0300

    23_4_1 improved

--- main.cpp
+++ main.cpp
@@ -19,15 +19,16 @@ using std::endl;
 #define SATURDAY_NAME "saturday"
 #define SUNDAY_NAME "sunday"
 
+#define CALL(func, arg) (func(arg))
 #define DONE(action, name) action ##_## name
 
-void get_monday() { cout << "This is: " MONDAY_NAME "\n"; }
-void get_tuesday() { cout << "This is: " TUESDAY_NAME "\n"; }
-void get_wednesday() { cout << "This is: " WEDNESDAY_NAME "\n"; }
-void get_thursday() { cout << "This is: " THURSDAY_NAME "\n"; }
-void get_friday() { cout << "This is: " FRIDAY_NAME "\n"; }
-void get_saturday() { cout << "This is: " SATURDAY_NAME "\n"; }
-void get_sunday() { cout << "This is: " SUNDAY_NAME "\n"; }
+void get_monday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_tuesday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_wednesday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_thursday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_friday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_saturday(const std::string &x) { cout << "This is: " << x << "\n"; }
+void get_sunday(const std::string &x) { cout << "This is: " << x << "\n"; }
 
 void get_day() {
     int userInput;
@@ -36,13 +37,15 @@ void get_day() {
         std::cin >> userInput;
 
         switch (userInput) {
-            case(MONDAY):    DONE(get, monday)(); break;
-            case(TUESDAY):   DONE(get, tuesday)(); break;
-            case(WEDNESDAY): DONE(get, wednesday)(); break;
-            case(THURSDAY):  DONE(get, thursday)(); break;
-            case(FRIDAY):    DONE(get, friday)(); break;
-            case(SATURDAY):  DONE(get, saturday)(); break;
-            case(SUNDAY):    DONE(get, sunday)(); break;
+            case(MONDAY):    DONE(get, monday)(MONDAY_NAME); break;
+            case(TUESDAY):   DONE(get, tuesday)(TUESDAY_NAME); break;
+            case(WEDNESDAY): DONE(get, wednesday)(WEDNESDAY_NAME); break;
+            case(THURSDAY):  DONE(get, thursday)(THURSDAY_NAME); break;
+            case(FRIDAY):    DONE(get, friday)(FRIDAY_NAME); break;
+            case(SATURDAY):  DONE(get, saturday)(SATURDAY_NAME); break;
+            // Можно даже так:
+            // case(SUNDAY):    DONE(get, sunday)(SUNDAY_NAME); break;
+            case(SUNDAY):    CALL(DONE(get, sunday), SUNDAY_NAME); break;
             default: cout << "Goodbye"; return;
         }
     }
